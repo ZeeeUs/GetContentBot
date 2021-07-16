8caf252bc294109b011b3a61e6df3c6e9023973f
Pogosyan Sos <pogosyan777.sp@gmail.com>
2021-07-16 12:45:03 +0300

    Start beta v 0.2.0

--- bot.py
+++ bot.py
@@ -1,65 +1,62 @@
+import requests.exceptions
 import telebot
 import config
 from telebot import types
-from scraper import start
-
+import main
 
 bot = telebot.TeleBot(config.configure['TOKEN'])
 
 
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    bot.send_message(message.chat.id, 'Привет! Этот бот поможет тебе скачать фото и видео из Instagram!\n'
-                                      'Если готов, то напиши "Поехали"', disable_web_page_preview=True)
+@bot.message_handler(commands=['/start', '/help'])
+def welcome_message(message):
+    bot.send_message(message.chat.id, "Привет! Этот бот поможет тебе скачать фото и видео из Instagram, "
+                                      "просто отправь ссылку!")
 
 
-@bot.message_handler(content_types=['text'])
-def handle_message(message):
-    if message.text.lower() == "поехали":
-        replay_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        replay_keyboard.add(types.KeyboardButton("Фото"), types.KeyboardButton("Видео"))
-        msg = bot.send_message(message.chat.id, "Что хочешь скачать: фото или видео?", reply_markup=replay_keyboard)
-        bot.register_next_step_handler(msg, user_answer)
-    else:
-        bot.send_message(message.chat.id, "Извини, мой функционал пока ограничени и я не до конца тебя понимаю ;(\n"
-                                          "Если тебе нужна помощь введи команду /help")
-
-
-def user_answer(message):
-    content_type = None
-    ext = None
-    replay_keyboard = types.ReplyKeyboardRemove(selective=False)
-    if message.text.lower() == "видео":
-        content_type = "video"
-        ext = "mp4"
-    elif message.text.lower() == "фото":
-        content_type = "image"
-        ext = "jpg"
-    msg = bot.send_message(message.chat.id, f"Отправь ссылку на {message.text.lower()}",
-                           reply_markup=replay_keyboard)
-    bot.register_next_step_handler(msg, download, content_type, ext)
-
-
-def download(message, content_type, ext):
-    error_message = "Упс! Что-то пошло не так, перепроверь ссылку.\n" \
-                    "Обрати внимание, что бот пока не может обрабатывать Reels'ы, " \
-                    "но мы это скоро исправим :)"
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def accept_url(message):
+    id_one = message.chat.id
     url = message.text
-    if "http://" in url or "https://" in url:
-        bot.send_message(message.chat.id, "Подожди пару секунд")
-        try:
-            content = start(content_type, ext, url)
-            print(content)
-            data = open(content, 'rb')
-            if content_type == "video":
-                bot.send_video(message.chat.id, data)
-            elif content_type == "image":
-                bot.send_photo(message.chat.id, data)
+    try:
+        all_url = main.start(url)
+        select_item(id_one, all_url)
+    except (AttributeError, requests.exceptions.MissingSchema):
+        bot.send_message(message.chat.id, "Упс! Что-то пошло не так, проверь ссылку и попробуй снова")
 
-        except (TypeError, AttributeError):
-            bot.send_message(message.chat.id, error_message)
+
+def select_item(id_one, all_url):
+    items_count = len(all_url.keys())
+    if items_count > 1:
+        replay_keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for i in range(items_count):
+            replay_keyboard.add(types.KeyboardButton(f"{i + 1}"))
+        replay_keyboard.add(types.KeyboardButton("Все"))
+        msg = bot.send_message(id_one, "Выбери что надо скачать", reply_markup=replay_keyboard)
+        bot.register_next_step_handler(msg, get_content, all_url)
     else:
-        bot.send_message(message.chat.id, error_message)
+        one_item(id_one, all_url)
+
+
+def one_item(id_one, all_url):
+    bot.send_message(id_one, "Подожди пару секунд")
+    content, ext = main.download(all_url)
+    switch(content, ext, id_one)
+
+
+def get_content(message, all_url):
+    replay_keyboard = types.ReplyKeyboardRemove(selective=False)
+    bot.send_message(message.chat.id, "Подожди пару секунд", reply_markup=replay_keyboard)
+    if message.text.lower() != "все":
+        content, ext = main.download(all_url[int(message.text) - 1])
+        switch(content, ext, message.chat.id)
+
+
+def switch(content, ext, id_one):
+    data = open("./media/" + content, 'rb')
+    if ext == "jpg":
+        bot.send_photo(id_one, data)
+    else:
+        bot.send_video(id_one, data)
 
 
 bot.polling(none_stop=True, interval=0)
---
+++ main.py
@@ -0,0 +1,62 @@
+import requests
+import random
+
+HOST = 'https://www.instagram.com/'
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'
+}
+
+
+def build_valid_url(url):
+    query_string = "__a=1"
+    url = url[:url.find("?") + 1] + query_string
+    return url
+
+
+def get_json(url):
+    r = requests.get(url, headers=HEADERS)
+    new_json = r.json()
+    return new_json
+
+
+def get_all_url(doc):
+    all_url = {}
+    try:
+        # Добираемся до контента
+        path_to_items = doc["graphql"]["shortcode_media"]["edge_sidecar_to_children"]["edges"]
+        # path_to_items = doc["graphql"]["shortcode_media"]["displays_resources"]
+        items_count = len(path_to_items)
+        for i in range(items_count):
+            # Проверка, явлется ли объект видео
+            if path_to_items[i]["node"]["is_video"]:
+                all_url[i] = {"mp4": path_to_items[i]["node"]["video_url"]}
+            else:
+                all_url[i] = {"jpg": path_to_items[i]["node"]["display_resources"][2]["src"]}
+    except KeyError:
+        path_to_items = doc["graphql"]["shortcode_media"]
+        if path_to_items["is_video"]:
+            all_url = {"mp4": path_to_items["video_url"]}
+        else:
+            all_url = {"jpg": path_to_items["display_resources"][2]["src"]}
+    return all_url
+
+
+def download(link):
+    ext = str()
+    if "mp4" in link:
+        ext = "mp4"
+    elif "jpg" in link:
+        ext = "jpg"
+    name = f'dwnld{random.randint(1, 1000)}.{ext}'
+    r = requests.get(link[ext])
+    with open("./media/" + name, 'wb') as f:
+        f.write(r.content)
+    return name, ext
+
+
+def start(user_url):
+    valid_url = build_valid_url(user_url)
+    doc = get_json(valid_url)
+    all_url = get_all_url(doc)
+    return all_url
