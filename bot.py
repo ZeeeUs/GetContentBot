6cc0bdfaaae2aa9012dc0b4515a80551f226c9da
Pogosyan Sos <pogosyan777.sp@gmail.com>
2021-07-17 16:45:31 +0300

    Test version

--- bot.py
+++ bot.py
@@ -3,8 +3,11 @@ import telebot
 import config
 from telebot import types
 import main
+import os
+from flask import Flask, request
 
 bot = telebot.TeleBot(config.configure['TOKEN'])
+server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -73,6 +76,23 @@ def switch(content, ext, id_one):
         bot.send_photo(id_one, data)
     else:
         bot.send_video(id_one, data)
+    os.remove("./media/" + content)
 
 
-bot.polling(none_stop=True, interval=0)
+@server.route('/' + config.configure['TOKEN'], methods=['POST'])
+def getMessage():
+    json_string = request.get_data().decode('utf-8')
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://getcontentbot.herokuapp.com/' + config.configure['TOKEN'])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
