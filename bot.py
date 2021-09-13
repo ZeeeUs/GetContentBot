02515efe064dabab2891981f3bf085581c4646f0
Pogosyan Sos <pogosyan.sos@wb.ru>
2021-09-13 16:31:41 +0300

    Readiness 80%

--- bot.py
+++ bot.py
@@ -4,10 +4,13 @@ import config
 from telebot import types
 import main
 import os
-from flask import Flask, request
+
+# from flask import Flask, request
 
 bot = telebot.TeleBot(config.configure['TOKEN'])
-server = Flask(__name__)
+
+
+# server = Flask(__name__)
 
 
 @bot.message_handler(commands=['start', 'help'])
@@ -79,20 +82,23 @@ def switch(content, ext, id_one):
     os.remove("./media/" + content)
 
 
-@server.route('/' + config.configure['TOKEN'], methods=['POST'])
-def getMessage():
-    json_string = request.get_data().decode('utf-8')
-    update = telebot.types.Update.de_json(json_string)
-    bot.process_new_updates([update])
-    return "!", 200
+# Задел для запуска на Webhook'ах
+# @server.route('/' + config.configure['TOKEN'], methods=['POST'])
+# def getMessage():
+#     json_string = request.get_data().decode('utf-8')
+#     update = telebot.types.Update.de_json(json_string)
+#     bot.process_new_updates([update])
+#     return "!", 200
+#
+#
+# @server.route("/")
+# def webhook():
+#     bot.remove_webhook()
+#     bot.set_webhook(url='https://getcontentbot.herokuapp.com/' + config.configure['TOKEN'])
+#     return "!", 200
+#
+#
+# if __name__ == "__main__":
+#     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
-
-@server.route("/")
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://getcontentbot.herokuapp.com/' + config.configure['TOKEN'])
-    return "!", 200
-
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+bot.polling(none_stop=True, interval=0)
